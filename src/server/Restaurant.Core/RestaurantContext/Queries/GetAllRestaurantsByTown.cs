92e833460330f037518e23ce8659fe35003c54e2
Jordan Jordanov <d_yugioh@abv.bg>
2019-04-23 14:53:18 +0300

    init

---
+++ src/server/Restaurant.Core/RestaurantContext/Queries/GetAllRestaurantsByTown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Restaurant.Core._Base;
+using Restaurant.Domain.Views.Restaurant;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.Core.RestaurantContext.Queries
+{
+    public class GetAllRestaurantsByTown : IQuery<IList<RestaurantWithAvrgRatingView>>
+    {
+        [Required]
+        public Guid TownId { get; set; }
+    }
+}
---
+++ src/server/Restaurant.Domain/SQL/RestaurantQueryRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant.Domain.SQL
+{
+    public class RestaurantQueryRepository
+    {
+        public const string RestaurantsByTownQuery =
+            @"SELECT res.""Id"", ""Name"", ROUND(AVG(rat.""Stars""),2) as ""AverageRating "", ""TownId""
+			  FROM public.""Restaurants"" as res
+			  INNER JOIN public.""Ratings"" as rat
+			  ON rat.""RestaurantId"" = res.""Id""
+			  WHERE ""TownId"" = @TownID   
+			  GROUP BY res.""Id"", ""Name"", ""TownId""";
+    }
+}
