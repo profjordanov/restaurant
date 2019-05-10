65e172b7973740ba547164f32199d3b4f5307b87
Jordan Jordanov <d_yugioh@abv.bg>
2019-05-10 22:49:25 +0300

    init

--- src/server/Restaurant.Domain/Entities/User.cs
+++ src/server/Restaurant.Domain/Entities/User.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Restaurant.Domain._Base;
+using Restaurant.Domain.Events.User;
 using System;
 using System.Collections.Generic;
-using Restaurant.Domain._Base;
 
 namespace Restaurant.Domain.Entities
 {
@@ -18,5 +19,33 @@ namespace Restaurant.Domain.Entities
         public virtual ICollection<Order> MadeOrders { get; set; } = new HashSet<Order>();
 
         public virtual ICollection<Restaurant> OwnedRestaurants { get; set; } = new HashSet<Restaurant>();
+
+        // Events
+        public UserLoggedIn LogInUser() => new UserLoggedIn
+        {
+            UserId = Id,
+            DateTime = DateTime.UtcNow
+        };
+
+        public UserRegistered RegisterUser() => new UserRegistered
+        {
+            UserId = Id,
+            FirstName = FirstName,
+            LastName = LastName,
+            RegistrationDate = RegistrationDate
+        };
+
+        public void Apply(UserLoggedIn @event)
+        {
+            Id = @event.UserId;
+        }
+
+        public void Apply(UserRegistered @event)
+        {
+            Id = @event.UserId;
+            FirstName = @event.FirstName;
+            LastName = @event.LastName;
+            RegistrationDate = @event.RegistrationDate;
+        }
     }
 }
---
+++ src/server/Restaurant.Domain/Events/User/UserRegistered.cs
@@ -0,0 +1,16 @@
+﻿using Restaurant.Domain.Events._Base;
+using System;
+
+namespace Restaurant.Domain.Events.User
+{
+    public class UserRegistered : IEvent
+    {
+        public Guid UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+}
