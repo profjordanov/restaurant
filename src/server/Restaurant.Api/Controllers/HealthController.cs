e6c4582188fe6b1049d326e8250dd48eb3db45f8
Jordan Jordanov <d_yugioh@abv.bg>
2019-05-20 23:47:17 +0300

    Create HealthController.cs

---
+++ src/server/Restaurant.Api/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Restaurant.Api is up and running!";
+    }
+}
