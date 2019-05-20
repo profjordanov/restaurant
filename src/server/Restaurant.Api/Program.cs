ff7b3904f0f70272622e9ef9ca9e7c0e93a24539
Jordan Jordanov <d_yugioh@abv.bg>
2019-05-20 23:59:20 +0300

    Initial

--- src/server/Restaurant.Api/Program.cs
+++ src/server/Restaurant.Api/Program.cs
@@ -8,11 +8,29 @@ namespace Restaurant.Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args, null).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, params string[] urls)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+            if (urls?.Length > 0)
+            {
+                builder.UseUrls(urls);
+            }
+
+            return builder;
+        }
+
+        /// <summary>
+        /// To be used by EF tooling until I implement IDesignTimeDbContextFactory.
+        /// https://wildermuth.com/2017/07/06/Program-cs-in-ASP-NET-Core-2-0
+        /// </summary>
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
---
+++ tests/Restaurant.Business.Tests/AppFixture.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurant.Api;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Restaurant.Business.Tests
+{
+    public class AppFixture
+    {
+        public static readonly string BaseUrl;
+        private static readonly IConfiguration _configuration;
+        private static readonly IServiceScopeFactory _scopeFactory;
+
+        static AppFixture()
+        {
+            BaseUrl = $"http://localhost:{GetFreeTcpPort()}";
+
+            var webhost = Program
+                .CreateWebHostBuilder(new[] { "--environment", "IntegrationTests" }, BaseUrl)
+                .Build();
+
+            webhost.Start();
+
+            var scopeFactory = (IServiceScopeFactory)webhost.Services.GetService(typeof(IServiceScopeFactory));
+
+            _scopeFactory = scopeFactory;
+
+            using (var scope = scopeFactory.CreateScope())
+            {
+                _configuration = scope.ServiceProvider.GetService<IConfiguration>();
+            }
+        }
+
+        private static int GetFreeTcpPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint) listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+    }
+}
