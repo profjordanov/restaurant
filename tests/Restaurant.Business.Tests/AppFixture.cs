b36048d39ecd574b25aeb88c42f85d90f8c345d3
Jordan Jordanov <d_yugioh@abv.bg>
2019-05-21 00:06:28 +0300

    Update AppFixture.cs

--- tests/Restaurant.Business.Tests/AppFixture.cs
+++ tests/Restaurant.Business.Tests/AppFixture.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurant.Api;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
+using System.Threading.Tasks;
+using MediatR;
+using Restaurant.Core._Base;
 
 namespace Restaurant.Business.Tests
 {
@@ -30,6 +35,74 @@ namespace Restaurant.Business.Tests
             {
                 _configuration = scope.ServiceProvider.GetService<IConfiguration>();
             }
+        }
+
+        public async Task ExecuteScopeAsync(Func<IServiceProvider, Task> action)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                try
+                {
+                    await action(scope.ServiceProvider).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public async Task<T> ExecuteScopeAsync<T>(Func<IServiceProvider, Task<T>> action)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                try
+                {
+                    var result = await action(scope.ServiceProvider).ConfigureAwait(false);
+
+                    return result;
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public string GetCompleteServerUrl(string route)
+        {
+            route = route.TrimStart('/', '\\');
+            return $"{BaseUrl}/{route}";
+        }
+
+        public Task SendManyAsync(params ICommand[] commands)
+        {
+            return ExecuteScopeAsync(async sp =>
+            {
+                var mediator = sp.GetService<IMediator>();
+
+                foreach (var command in commands)
+                {
+                    await mediator.Send(command);
+                }
+
+                return Task.CompletedTask;
+            });
+        }
+
+        private static HttpClient BuildHttpClient(string accessToken = null)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+            }
+
+            return client;
         }
 
         private static int GetFreeTcpPort()
