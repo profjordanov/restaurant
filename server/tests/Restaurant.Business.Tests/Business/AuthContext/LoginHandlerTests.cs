88be506071ad5b948971c890b6c65a4bcfc95517
Jordan Jordanov <d_yugioh@abv.bg>
2019-06-12 10:48:37 +0300

    RegisterHandlerTests & LoginHandlerTests
    Integration tests over RegisterHandler & LoginHandler

--- server/tests/Restaurant.Business.Tests/Business/AuthContext/LoginHandlerTests.cs
+++ server/tests/Restaurant.Business.Tests/Business/AuthContext/LoginHandlerTests.cs
@@ -1,5 +1,7 @@
 ﻿using Restaurant.Business.Tests.Customizations;
+using Restaurant.Business.Tests.Extensions;
 using Restaurant.Core.AuthContext.Commands;
+using Restaurant.Domain.Enumerations;
 using Shouldly;
 using System;
 using System.IdentityModel.Tokens.Jwt;
@@ -10,6 +12,7 @@ namespace Restaurant.Business.Tests.Business.AuthContext
 {
     public class LoginHandlerTests : ResetDatabaseLifetime
     {
+        private const string RegisterUserCommandEmail = "test@email.com";
         private readonly AppFixture _fixture;
 
         public LoginHandlerTests()
@@ -22,7 +25,7 @@ namespace Restaurant.Business.Tests.Business.AuthContext
         public async Task CanLogin(Register registerUserCommand)
         {
             // Arrange
-            registerUserCommand.Email = "test@email.com";
+            registerUserCommand.Email = RegisterUserCommandEmail;
             await _fixture.SendAsync(registerUserCommand);
 
             var loginCommand = new Login
@@ -37,6 +40,55 @@ namespace Restaurant.Business.Tests.Business.AuthContext
             // Assert
             // TODO: Validate subject, claims, etc.
             result.Exists(jwt => IsValidJwtString(jwt.TokenString)).ShouldBeTrue();
+        }
+
+        [Theory]
+        [CustomizedAutoData]
+        public async Task CannotLoginWithInvalidCredentials(Register registerUserCommand)
+        {
+            // Arrange
+            registerUserCommand.Email = RegisterUserCommandEmail;
+            await _fixture.SendAsync(registerUserCommand);
+
+            var loginCommand = new Login
+            {
+                Email = registerUserCommand.Email,
+                Password = registerUserCommand.Password + "123" // Must be different
+            };
+
+            // Act
+            var result = await _fixture.SendAsync(loginCommand);
+
+            // Assert
+            result.ShouldHaveErrorOfType(ErrorType.Unauthorized);
+        }
+
+        [Theory]
+        [CustomizedAutoData]
+        public async Task CannotLoginWithInvalidEmail(Login command)
+        {
+            // Arrange
+            command.Email = "invalid-email";
+
+            // Act
+            var result = await _fixture.SendAsync(command);
+
+            // Assert
+            result.ShouldHaveErrorOfType(ErrorType.Validation);
+        }
+
+        [Theory]
+        [CustomizedAutoData]
+        public async Task CannotLoginWithNonExistingEmail(Login command)
+        {
+            // Arrange
+            // We are not registering any users so the email is definitely not going to be found
+
+            // Act
+            var result = await _fixture.SendAsync(command);
+
+            // Assert
+            result.ShouldHaveErrorOfType(ErrorType.Validation);
         }
 
         private static bool IsValidJwtString(string tokenString)
