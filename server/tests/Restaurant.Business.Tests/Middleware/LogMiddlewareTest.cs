c87b8c93145912979352956d9ef948883d407549
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-04 21:16:37 +0300

    Create LogMiddlewareTest.cs

---
+++ server/tests/Restaurant.Business.Tests/Middleware/LogMiddlewareTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Restaurant.Api.Middlewares;
+using Restaurant.Persistence.Connectors;
+using Xunit;
+
+namespace Restaurant.Business.Tests.Middleware
+{
+    public class LogMiddlewareTest
+    {
+        [Fact]
+        public async Task Test()
+        {
+            var mid = new LogMiddleware(
+                context => Task.CompletedTask, new AsyncDatabaseLogger());
+        }
+    }
+}
